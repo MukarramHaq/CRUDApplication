76c477ea10399e345101f69293fc2d2713c8097b
Mukarram Haq <mukarramhaq@mukarrams-air.mynetworksettings.com>
2023-11-18 15:46:17 -0500

    Checking

--- src/main/java/com/example/CRUDApplication/Controller.java
+++ src/main/java/com/example/CRUDApplication/Controller.java
@@ -2,10 +2,14 @@ package com.example.CRUDApplication;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +22,13 @@ public class Controller {
         List<Books> bookList = new ArrayList<>();
         bookRepo.findAll().forEach(bookList::add);
         if(bookList.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(bookList, HttpStatus.NO_CONTENT);
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
+ 
+    
     public void getBookById(){
 
     }
@@ -39,5 +44,5 @@ public class Controller {
     public void deleteBookById(){
 
     }
-
+    
 }
